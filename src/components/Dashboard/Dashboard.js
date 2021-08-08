20afc0297b0b9a42c3624b0c09d1a1fb129741e7
Hussein Barakat <h.barakat@totersapp.com>
2021-08-08 09:31:52 +0300

    display customers

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,6 +3,8 @@ import { UserService } from '../../services/UserService';
 
 const Dashboard = (props) => {
 
+    let [customers, setCustomers] = useState(null);
+
     useEffect(()=>{
         const params = {
             token: localStorage.getItem('token'),
@@ -10,17 +12,44 @@ const Dashboard = (props) => {
         }
 
         UserService.getCustomers(params).then(res => {
-            console.log(res)
+            if (res.customers) {
+                setCustomers(res.customers)
+            }
         }).catch(error => console.log(error))
 
 
         UserService.getAverageCustomers(params).then(res => {
-            console.log(res)
+            // console.log(res)
         }).catch(error => console.log(error))
     },[]) 
 
+    console.log(customers)
     return (
-        <div>asdasdasf</div>
+        <div className="container-fluid m-4">
+            <table className="table table-striped border" >
+                <thead>
+                    <tr className="pl-4 text-center">
+                        <th >First Name</th>
+                        <th >Last Name</th>
+                        <th >Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {(customers) ?  
+                    Object.entries(customers.data).map(([key, val], index) => {
+                        return (
+                            <tr key={index} className='text-center'>
+                                <td>{val.first_name}</td>
+                                <td>{val.last_name}</td>
+                                <td>{val.email}</td>
+                            </tr>
+                        )})
+                        :
+                        <div></div>
+                    } 
+                </tbody>
+            </table>
+        </div>
     );
 }
 
