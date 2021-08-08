17d09971be3c48422f988cd513b4579d29f5e575
Hussein Barakat <h.barakat@totersapp.com>
2021-08-08 11:36:50 +0300

    redirect after user is logged in

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { UserService } from '../../services/UserService';
+import {
+    Redirect
+  } from "react-router-dom";
 
 const Login = (props) => {
 
     let [email, setEmail] = useState("");
     let [password, setPassword] = useState("");
+
+    let [redirectTo, setRedirectTo] = useState('/login');
 
     const handleEmail = (e) => {
        setEmail(e.target.value);
@@ -28,30 +33,39 @@ const Login = (props) => {
             else {
                 localStorage.setItem("isAuthenticated", "true");
                 localStorage.setItem("token", res.token);
-                props.onChange(true)
+                setRedirectTo('/dashboard');
+                props.onChange(true);
             }
         }).catch(error => console.log(error))
     }
+
+    console.log(redirectTo)
     
     return (
         <div>
-            <div className="container w-25 text-center mt-5">
-                <h1>Login</h1>
-                <form onSubmit={login}>
-                    <div className="m-3 form-group">
-                        <input className="form-control" type="text" placeholder="Email" onChange={handleEmail} />
-                    </div>
+            {
+            redirectTo !== '/login' ?
+                <Redirect to={redirectTo} />
+            :
+                <div className="container w-25 text-center mt-5">
+                    <h1>Login</h1>
+                    <form onSubmit={login}>
+                        <div className="m-3 form-group">
+                            <input className="form-control" type="text" placeholder="Email" onChange={handleEmail} />
+                        </div>
 
-                    <div className="m-3 form-group">
-                        <input className="form-control" type="password" placeholder="Password" onChange={handlePassword} />
-                    </div>
+                        <div className="m-3 form-group">
+                            <input className="form-control" type="password" placeholder="Password" onChange={handlePassword} />
+                        </div>
 
-                    <div>
-                        <button className="btn btn-primary text-white">Login</button>
-                    </div>
-                </form>
-            </div>
+                        <div>
+                            <button className="btn btn-primary text-white">Login</button>
+                        </div>
+                    </form>
+                </div>
+            }
         </div>
+
     )
 
 }
