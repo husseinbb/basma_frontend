6b156f867511ed8c2b41c0cdd58651797b151c6a
Hussein Barakat <h.barakat@totersapp.com>
2021-08-09 19:54:48 +0300

    remove unused import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Login from './components/Login/Login';
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
