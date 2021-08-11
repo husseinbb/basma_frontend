8915518524b6da17747d910648ef5fc0aa372f06
Hussein Barakat <h.barakat@totersapp.com>
2021-08-11 12:39:42 +0300

    addadd heroku base url

--- src/services/ConstantsService.js
+++ src/services/ConstantsService.js
@@ -1,4 +1,5 @@
 export class ConstantsService {
 
-    static baseUrl = "http://localhost:8000/api";
+    // static baseUrl = "http://localhost:8000/api";
+    static baseUrl = "https://evening-fjord-11570.herokuapp.com/api";
 }
